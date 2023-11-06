4995b45517d1066862947200d429bc2ebe02eadc
Klemen <klemen.skok1@gmail.com>
2023-11-06 20:41:56 +0100

    dodana 4. naloga na 2. listu (nedokoncana)

---
+++ List_2/Kseznami2_4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+struct El {
+    char ime[20], 
+    priimek[30], 
+    email[60], 
+    gsm[9];
+    struct El *prev, *next;
+} *start = nullptr, *konc = nullptr;
+
+int main() {
+
+
+
+    return 0;
+}
